ee22075db2990795ccc66cb4ed6fbe67a9b1285d
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-09-10 17:48:33 -0700

    compares the two different values I assigned the variables and prints out the biggest of the two

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,17 +5,27 @@ int main()
 {
         int number1;
         int number2;
-
-  std::cout<<"27:\n";
+        int biggest; 
+  std::cout<<"please enter a whole number:\n";
   //get first input using std::cin>>
+  std::cin>>number1;
 
-  std::cout<<"28:\n";
+  std::cout<<"please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-
+  std::cin>>number2;
   //determine if first or second is bigger and store that in a third variable
+  if(number1  > number2)
+  {
+        biggest=number1;
+  }
+else
+{
+
+        biggest=number2;
+}        
 
 
-  std::cout<<"Of those two numbers, the biggest is: ";
+  std::cout<<"Of those two numbers, the biggest is: "<<biggest;
   //print out the variable representing the biggest number
 
   std::cout<<std::endl<<"Thank you for playing.\n";
